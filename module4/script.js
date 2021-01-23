186905934560cfe9171eebc09501691407befa4a
tchsource <72528240+tchsource@users.noreply.github.com>
2021-01-23 11:02:27 -0600

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -38,7 +38,7 @@ WARNING!!! WARNING!!!
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for ( var i=0; i<names.length;i++) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -47,17 +47,18 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+   var lowercasename = names[i].toLowerCase();
+   var firstLetter = lowercasename.charAt(0));
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (firsLetter == "j") {
+     byeSpeaker.speak(names[i])
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i])
   }
 }
      }) (window);
